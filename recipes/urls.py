03d4d509d42c0f7a96674e51019284be9d2ef51c
Rafael Calassara <rafaelcalassara@hotmail.com>
2024-06-30 17:46:07 -0300

    criado recipe partials

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,4 @@ from recipes.views import *
 
 urlpatterns = [
     path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -4,10 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
+    return render(request, 'recipes/pages/home.html')
