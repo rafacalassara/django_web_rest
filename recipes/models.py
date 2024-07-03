fe656e3d751be19d33ac03f23a6ce016c620576c
Rafael Calassara <rafaelcalassara@hotmail.com>
2024-07-03 18:30:46 -0300

    terminado recipe.models e feitas migrations

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -16,5 +19,10 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-    
-    
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
+
