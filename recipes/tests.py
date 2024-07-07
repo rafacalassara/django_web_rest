2fc9b6459547336f7ea642d29350416e6fa0999f
rafaelcalassara <rafaelcalassara@hotmail.com>
2024-07-07 14:59:03 -0300

    criados arquivos separados para testes

--- recipes/tests.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-
-class RecipesURLsTests(TestCase):
-    def test_the_pytest_is_ok(self):
-        assert 1 == 2, 'Um nao e igual a dois'
---
+++ recipes/tests/test_recipe_views.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+from recipes import views
+
+class RecipeViewTests(TestCase):
+    def test_recipe_home_views_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+    def test_recipe_category_views_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:category', kwargs={'category_id': 1})
+        )
+        self.assertIs(view.func, views.category)
+    def test_recipe_detail_views_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:recipe', kwargs={'id': 1})
+        )
+        self.assertIs(view.func, views.recipes)   
+
