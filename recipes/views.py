ab83b74645fed84f57809699119d33cedc1a546d
rafaelcalassara <rafaelcalassara@hotmail.com>
2024-07-07 12:30:45 -0300

    update no filtro das views para shortcuts django

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 
@@ -13,22 +13,28 @@ def home(request):
     }) 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
+    queryset = Recipe.objects.filter(
         category__id=category_id,
         is_published=True
     ).order_by('-id')
+
+    recipes = get_list_or_404(queryset)
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes':recipes,
-        'title': f'{recipes.first().category.name} - Category | '
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
 def recipes(request, id):
-    recipe = Recipe.objects.filter(
+    queryset = Recipe.objects.filter(
         is_published=True,
         pk=id
     )
+
+    recipe = get_object_or_404(queryset)
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': recipe.first(),
+        'recipe': recipe,
         'is_detail_page':True,
     })
 
