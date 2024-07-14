2c15c57fc212b9636c9abc5fbb059b8ef9cc23bc
rafaelcalassara <rafaelcalassara@hotmail.com>
2024-07-14 15:26:33 -0300

    add test search 404

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,21 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
-from utils.recipes.factory import make_recipe
+from django.http import Http404
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+
 from recipes.models import Recipe
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
+
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
-    
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes':recipes,
-    }) 
+        'recipes': recipes,
+    })
+
 
 def category(request, category_id):
     queryset = Recipe.objects.filter(
@@ -22,9 +26,10 @@ def category(request, category_id):
     recipes = get_list_or_404(queryset)
 
     return render(request, 'recipes/pages/category.html', context={
-        'recipes':recipes,
+        'recipes': recipes,
         'title': f'{recipes[0].category.name} - Category | '
     })
+
 
 def recipe(request, id):
     queryset = Recipe.objects.filter(
@@ -36,9 +41,12 @@ def recipe(request, id):
 
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
-        'is_detail_page':True,
+        'is_detail_page': True,
     })
 
-def search(request):
-    return render(request, 'recipes/pages/search.html')
 
+def search(request):
+    search_term = request.GET.get('q')
+    if not search_term:
+        raise Http404()
+    return render(request, 'recipes/pages/search.html')
